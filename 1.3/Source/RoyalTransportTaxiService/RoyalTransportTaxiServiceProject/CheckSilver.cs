7eb3586ab74a97769203c25098e2047f02317c42
Kevin Bromley <kevin.bromley@gmail.com>
2022-04-25 13:15:39 -0400

    fixed the project file framework to 4.7.2

---
+++ 1.3/Source/RoyalTransportTaxiService/RoyalTransportTaxiServiceProject/CheckSilver.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+
+namespace RoyalTransportTaxiService
+{
+    static class BromsCheckSilver // was util from call traders mod
+    {
+        public static bool HasEnoughSilver(Map map, out int found)
+        {
+            found = 0;
+
+            int need = 10 // was Settings.Cost;
+            if (need == 0)
+                return true;
+
+            foreach (Thing t in TradeUtility.AllLaunchableThingsForTrade(map))
+            {
+                if (t.def == ThingDefOf.Silver)
+                {
+                    found += t.stackCount;
+                    if (found >= need)
+                        return true;
+                }
+            }
+            return false;
+        }
+    }
+}
